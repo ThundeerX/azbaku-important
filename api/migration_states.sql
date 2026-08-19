b4fd66045f6a01d0042dfd6a5c83fde5481e8365
Thundeer <130177783+ThundeerX@users.noreply.github.com>
2026-08-19 12:56:33 +0400

    Delete api directory

--- api/migration_states.sql
+++
@@ -1,9 +0,0 @@
--- Auksion + ştat üzrə ABŞ daxili daşınma tarifləri
-CREATE TABLE IF NOT EXISTS shipping_states (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  auction VARCHAR(30) NOT NULL,
-  state_name VARCHAR(80) NOT NULL,
-  port VARCHAR(80),
-  cost INT DEFAULT 250,
-  UNIQUE KEY uq_auction_state (auction, state_name)
-) DEFAULT CHARSET=utf8mb4;
